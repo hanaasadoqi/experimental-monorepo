ef1f6e59afeb1f669a5678f9b58722ce4664d82d
Hanaa Sadoqi <hanaa.sadoqi@gmail.com>
2026-08-29 02:09:11 -0400

    fix: update prettier config import path and remove unnecessary export semicolon

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,3 @@
-import tailwindConfig from '@repo/foundatio-prettier-config/tailwind'
+import tailwindConfig from "@repo/foundation-prettier-config/tailwind"
 
-export default tailwindConfig;
+export default tailwindConfig
